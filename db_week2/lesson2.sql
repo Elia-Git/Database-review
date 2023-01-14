5c345d31b2a8c711f80adbf54b089de6c35d235e
Elia-Git <holeselv17@gmail.com>
2023-01-15 00:02:07 +0100

    week2 class23

--- db_week2/lesson2.sql
+++ db_week2/lesson2.sql
@@ -1,7 +1,3 @@
-
-use db_week2;
-SELECT*from task;
-SELECT*from user;
 -- Add new column into task table
 ALTER TABLE   task
 ADD   user_id int(10) unsigned NOT NULL;
@@ -13,10 +9,9 @@ JOIN user_task ON task.id = user_task.task_id
 JOIN user ON user.id = user_task.user_id
 WHERE user.email LIKE '%@spotify.com';
 --Or
-SELECT
-user.name,user.email, task.title
+SELECT user.name,user.email, task.title
 FROM task
-    LEFT JOIN user ON task.user_id = user.id
+LEFT JOIN user ON task.user_id = user.id
 WHERE
     user.email LIKE '%apolak4@indiatimes.com';
 
@@ -32,12 +27,10 @@ SELECT task.id,
     user.name AS 'user',
     status.name AS status
 FROM user_task
-    JOIN task ON user_task.task_id = task.id
-    JOIN user ON user_task.user_id = user.id
-    JOIN status ON task.status_id = status.id
-WHERE
-    status.name = "Not started"
-    AND user.name = 'Donald Duck';
+JOIN task ON user_task.task_id = task.id
+JOIN user ON user_task.user_id = user.id
+JOIN status ON task.status_id = status.id
+WHERE status.name = "Not started" AND user.name = 'Donald Duck';
 
 -- Get all the tasks for 'Maryrose Meadows' that were created in september (hint: month(created)=month_number)
 SELECT task.id,user.name, task.title, task.created
@@ -45,8 +38,7 @@ FROM task
 JOIN user_task ON task.id = user_task.task_id
 JOIN user  ON user.id = user_task.user_id
 WHERE user.name LIKE 'Maryrose Meadows' AND MONTH(created)=09;
-
-
 -- Find how many tasks where created in each month,
-SELECT COUNT(id) as nrOfMonthlyTasks ,MONTHNAME(created) as "month" FROM task
+SELECT COUNT(id) as nrOfMonthlyTasks ,MONTHNAME(created) as "month"
+FROM task
 GROUP BY month;
