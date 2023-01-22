cd6df971fc237ad89a52aa2ad501b4a9f50acf77
Elia-Git <holeselv17@gmail.com>
2023-01-22 23:43:24 +0100

    updates...

--- db_week2/school_db.sql
+++ db_week2/school_db.sql
@@ -1,7 +1,7 @@
 -- creating class table
 CREATE TABLE `class` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` varchar(255) NOT NULL,
+  `name` varchar(15) NOT NULL,
   `begins` date NULL,
   `ends` date NULL
   );
@@ -9,9 +9,9 @@ CREATE TABLE `class` (
   -- creating student table
 CREATE TABLE `student` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NULL,
-  `phone` varchar(255) NULL,
+  `name` varchar(15) NOT NULL,
+  `email` varchar(25) NULL,
+  `phone` varchar(15) NULL,
   `class_id` int(10) unsigned NOT NULL,
   CONSTRAINT `fk_class` FOREIGN KEY (`class_id`) REFERENCES `class` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
   );
